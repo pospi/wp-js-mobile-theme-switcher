72265eacb98bde0bf8d79f5d9062ee2e6f356854
pospi <pospi@spadgos.com>
2013-06-19 03:50:39 +0100

    added JS to handle once-per-visit redirection option

--- mobile-theme-switch.js
+++ mobile-theme-switch.js
@@ -4,7 +4,8 @@
 		return;
 	}
 
-	var FLAG_MOBILE = 'm',
+	var CHECKED_COOKIE_NAME = 'jsmts_checked',
+		FLAG_MOBILE = 'm',
 		FLAG_TABLET = 't',
 		FLAG_DESKTOP = 'd';
 
@@ -56,7 +57,47 @@
 		return str.substring(0, query) + paramStr.join('&');
 	}
 
+	// cookie manipulation methods taken from http://www.quirksmode.org/js/cookies.html
+
+	function createCookie(name, value, days)
+	{
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			var expires = "; expires="+date.toGMTString();
+		}
+		else var expires = "";
+		document.cookie = name+"="+value+expires+"; path=/";
+	}
+
+	function readCookie(name)
+	{
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	}
+
+	function eraseCookie(name)
+	{
+		createCookie(name,"",-1);
+	}
+
 	// BEGIN MAIN LOGIC
+
+	if (JSMTS.set_state) {
+		// stop processing if we've already checked for a browser this session
+		if (readCookie(CHECKED_COOKIE_NAME)) {
+			return;
+		}
+
+		// flag a cookie if we have now checked
+		createCookie(CHECKED_COOKIE_NAME, 1, 0);
+	}
 
 	// parse useragent to sniff platform
 
